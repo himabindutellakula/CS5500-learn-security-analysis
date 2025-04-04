735877bf3132832c8f3f23f640acada5b017f184
himabindutellakula <tellakulahimabindu26@gmail.com>
2025-04-04 14:59:07 -0400

    Adding solution

--- pages/create_book.ts
+++ pages/create_book.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import Book from '../models/book';
 import express from 'express';
 import bodyParser from 'body-parser';
+import { validateBookDetailsMiddleware, RequestWithSanitizedBookDetails } from '../sanitizers/bookSanitizer';
 
 const router = express.Router();
 
@@ -17,19 +18,17 @@ router.use(express.json());
  * @route POST /newbook
  * @returns a newly created book for an existing author and genre in the database
  * @returns 500 error if book creation failed
+ * @returns 400 error if the input validation fails
  */
-router.post('/', async (req: Request, res: Response) => {
+router.post('/', validateBookDetailsMiddleware, async (req: Request, res: Response) => {
   const { familyName, firstName, genreName, bookTitle } = req.body;
-  if (familyName && firstName && genreName && bookTitle) {
-    try {
-      const book = new Book({});
-      const savedBook = await book.saveBookOfExistingAuthorAndGenre(familyName, firstName, genreName, bookTitle);
-      res.status(200).send(savedBook);
-    } catch (err: unknown) {
-      res.status(500).send('Error creating book: ' + (err as Error).message);
-    }
-  } else {
-    res.send('Invalid Inputs');
+  try {
+    const book = new Book({});
+    const savedBook = await book.saveBookOfExistingAuthorAndGenre(familyName, firstName, genreName, bookTitle);
+    res.status(200).send(savedBook);
+  } catch (err: unknown) {
+    console.error('Error creating book:', (err as Error).message);
+    res.status(500).send('Error creating book: ' + bookTitle);
   }
 });
 
